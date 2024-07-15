a0dfc08886da1bed8eefbf30089572833c88986f
Turbobot <turbobot@vercel.com>
2024-07-16 02:30:01 +0800

    chore: setup basic backend deps

--- packages/eslint-config/backend.js
+++ packages/eslint-config/backend.js
@@ -4,21 +4,11 @@ const project = resolve(process.cwd(), 'tsconfig.json');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'prettier',
-		require.resolve('@vercel/style-guide/eslint/next'),
-		'turbo',
-	],
-	globals: {
-		React: true,
-		JSX: true,
-	},
+	extends: ['eslint:recommended', 'prettier', 'turbo'],
+	plugins: ['only-warn'],
 	env: {
 		node: true,
-		browser: true,
 	},
-	plugins: ['only-warn'],
 	settings: {
 		'import/resolver': {
 			typescript: {
@@ -30,6 +20,11 @@ module.exports = {
 		// Ignore dotfiles
 		'.*.js',
 		'node_modules/',
+		'dist/',
 	],
-	overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
+	overrides: [
+		{
+			files: ['*.js?(x)', '*.ts?(x)'],
+		},
+	],
 };
