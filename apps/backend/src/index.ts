4613c4b3343de18254e8afea694f9400b4a81236
Nixieboluo <me@sagirii.me>
2024-07-17 23:50:58 +0800

    feat(backend): add swagger

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,7 @@
+import swagger from '@elysiajs/swagger';
+import { AuthController } from './routes/auth/auth.controller';
 import { Elysia } from 'elysia';
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+const app = new Elysia().use(swagger()).listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(`Service is running at ${app.server?.hostname}:${app.server?.port}`);
