e053749599c8a5220799d9a999d46d94466b6d91
Nixieboluo <me@sagirii.me>
2024-07-17 21:47:14 +0800

    feat(backend): use cuid2 for ids and add timestamps for user table

--- apps/backend/src/db/schema/auth.ts
+++ apps/backend/src/db/schema/auth.ts
@@ -1,16 +1,16 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { createId } from '@paralleldrive/cuid2';
+import { pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
-	id: text('id').primaryKey(),
+	id: varchar('id', { length: 24 }).primaryKey().$defaultFn(createId),
+	createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+	updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date()),
 });
 
 export const sessionTable = pgTable('session', {
-	id: text('id').primaryKey(),
-	userId: text('user_id')
+	id: varchar('id', { length: 24 }).primaryKey().$defaultFn(createId),
+	userId: varchar('id', { length: 24 })
 		.notNull()
 		.references(() => userTable.id),
-	expiresAt: timestamp('expires_at', {
-		withTimezone: true,
-		mode: 'date',
-	}).notNull(),
+	expiresAt: timestamp('expires_at', { withTimezone: true }).notNull(),
 });
