c0cbf118cdc067a38f5c4d9fbfa1449a074ab668
Nixieboluo <me@sagirii.me>
2024-07-17 22:37:10 +0800

    chore(backend): database migration should have only one connetction

--- apps/backend/src/db/migrate.ts
+++ apps/backend/src/db/migrate.ts
@@ -2,6 +2,6 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-const queryClient = postgres(process.env.DATABASE_URL_MIGRATE, {});
+const queryClient = postgres(process.env.DATABASE_URL_MIGRATE, { max: 1 });
 
 migrate(drizzle(queryClient), { migrationsFolder: 'migrations' }).finally(() => queryClient.end());
