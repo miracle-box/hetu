aa4f8e1a7662f2860348cb34fba042a335b8ac91
Nixieboluo <me@sagirii.me>
2024-07-19 22:14:12 +0800

    feat(backend): add texture table

---
+++ apps/backend/src/db/schema/texture.ts
@@ -0,0 +1,18 @@
+import { createId } from '@paralleldrive/cuid2';
+import { pgEnum, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { userTable } from './auth';
+
+export const TextureTypeEnum = pgEnum('texture_type', ['skin', 'skin_slim', 'cape']);
+
+export const textureTable = pgTable('texture', {
+	id: varchar('id', { length: 24 }).primaryKey().$defaultFn(createId),
+	authorId: varchar('author_id', { length: 24 })
+		.notNull()
+		.references(() => userTable.id),
+	name: varchar('name').notNull(),
+	description: text('description').notNull().default(''),
+	type: TextureTypeEnum('type').notNull(),
+	hash: varchar('hash').notNull(),
+	createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+	updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date()),
+});
