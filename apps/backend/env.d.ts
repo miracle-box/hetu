28df3a1b52b860c05515cd912323de8626792be1
Nixieboluo <me@sagirii.me>
2024-07-28 18:14:14 +0800

    feat(backend): configure s3

--- apps/backend/env.d.ts
+++ apps/backend/env.d.ts
@@ -2,5 +2,9 @@ declare module 'bun' {
 	interface Env {
 		DATABASE_URL: string;
 		DATABASE_URL_MIGRATE: string;
+		S3_ENDPOINT: string;
+		S3_BUCKET: string;
+		S3_ACCESS_KEY_ID: string;
+		S3_SECRET_ACCESS_KEY: string;
 	}
 }
