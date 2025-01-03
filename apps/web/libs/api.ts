215c6e7b71ad7dce406fd91c226cf65aa1ff80f7
Nixieboluo <me@sagirii.me>
2025-01-03 20:46:58 +0800

    feat(web): configure radix themes

--- apps/web/libs/api.ts
+++ apps/web/libs/api.ts
@@ -1,4 +1,4 @@
 import { treaty } from '@elysiajs/eden';
-import type { App } from '@repo/backend';
+import type { App } from '~backend/index';
 
 export const client: ReturnType<typeof treaty<App>> = treaty<App>(process.env.API_ROOT);
