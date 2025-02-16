87847dcc5efe4d2927f3b5b3528e2debf522ee8b
Nixieboluo <me@sagirii.me>
2025-02-16 15:20:26 +0800

    build(backend): bundle libvips when building

--- apps/backend/scripts/build.ts
+++ apps/backend/scripts/build.ts
@@ -10,29 +10,15 @@ function bunPluginEmbedSharpNative(): BunPlugin {
 			const green = (str: string) => `\u001b[32m${str}\u001b[0m`;
 
 			// @ts-expect-error Sharp libvips module does not have type declarations.
-			const libvipsModule = await import('sharp/lib/libvips.js').catch(() => {
+			const libvipsHelper = await import('sharp/lib/libvips.js').catch(() => {
 				console.log(
 					green('Sharp is not detected, skipping native module loader injection.'),
 				);
 			});
-			if (!libvipsModule) return;
-
-			const platform: string = libvipsModule.runtimePlatformArch();
+			if (!libvipsHelper) return;
+			const platform: string = libvipsHelper.runtimePlatformArch();
 
 			console.log(green('Bundling Sharp native modules...'));
-
-			const sharpNativePath = Bun.resolveSync(
-				`@img/sharp-${platform}/sharp.node`,
-				import.meta.dir,
-			);
-			await Bun.build({
-				entrypoints: [sharpNativePath],
-				outdir: build.config.outdir,
-				target: build.config.target,
-				naming: build.config.naming,
-				sourcemap: build.config.sourcemap,
-				minify: build.config.minify,
-			});
 
 			// [TODO] Support targets other than bun in the future
 			if (build.config.target !== 'bun')
@@ -40,14 +26,59 @@ function bunPluginEmbedSharpNative(): BunPlugin {
 					'Sharp native bundler plugin not supports targets other than bun yet.',
 				);
 
+			const sharpNativePath = Bun.resolveSync(
+				`@img/sharp-${platform}/sharp.node`,
+				import.meta.dir,
+			);
+			await Bun.build({
+				entrypoints: [sharpNativePath],
+				outdir: path.join(
+					build.config.outdir ?? '',
+					`/sharp_natives/sharp/sharp-${platform}`,
+				),
+				target: build.config.target,
+				naming: build.config.naming,
+				sourcemap: build.config.sourcemap,
+				format: build.config.format,
+				minify: build.config.minify,
+			});
+
+			// libvips is under @img/sharp-win32-{arch} on Windows
+			const libvipsSrcDir = platform.startsWith('win32')
+				? path.dirname(sharpNativePath)
+				: path.dirname(
+						Bun.resolveSync(`@img/sharp-libvips-${platform}/lib`, import.meta.dir),
+					);
+			const libvipsDestDir = platform.startsWith('win32')
+				? path.join(build.config.outdir ?? '', `/sharp_natives/@img/sharp-${platform}/lib`)
+				: path.join(
+						build.config.outdir ?? '',
+						`/sharp_natives/sharp-libvips-${platform}/lib`,
+					);
+
+			await fs.mkdir(libvipsDestDir, { recursive: true });
+			await fs
+				.readdir(libvipsSrcDir)
+				.then((files) =>
+					files
+						.filter((file) => /^libvips.*\.(so|dll|dylib)/.test(file))
+						.map((file) =>
+							fs.copyFile(
+								path.join(libvipsSrcDir, file),
+								path.join(libvipsDestDir, file),
+							),
+						),
+				)
+				.then((tasks) => Promise.all(tasks));
+
 			let injected = false;
-			build.onLoad({ filter: /\/lib\/sharp.js$|\\lib\\sharp.js$/ }, async (args) => {
+			build.onLoad({ filter: /[/\\]lib[/\\]sharp.js$$/ }, async (args) => {
 				if (injected) return;
 				injected = true;
 
 				const contents = (await Bun.file(args.path).text()).replace(
 					'const paths = [',
-					`const paths = [\n  Bun.resolveSync(\`./sharp-${platform}.js\`, import.meta.dir),\n`,
+					`const paths = [\n  Bun.resolveSync(\`./sharp_natives/sharp/sharp-${platform}/sharp-${platform}.js\`, import.meta.dir),\n`,
 				);
 
 				console.log(green('Sharp native module path injected.'));
@@ -58,17 +89,22 @@ function bunPluginEmbedSharpNative(): BunPlugin {
 	};
 }
 
+await fs.rm('./dist', {
+	recursive: true,
+	force: true,
+});
+
 // [TODO] Compiling into binaries is not supported by the Bun.build API
 // See: https://github.com/oven-sh/bun/issues/11895
 await Bun.build({
 	entrypoints: ['./src/index.ts'],
 	target: 'bun',
 	outdir: './dist',
-	// minify: {
-	// 	syntax: true,
-	// 	whitespace: true,
-	// 	identifiers: false,
-	// },
+	minify: {
+		syntax: true,
+		whitespace: true,
+		identifiers: false,
+	},
 	sourcemap: 'linked',
 	plugins: [
 		bunPluginEmbedSharpNative(),
