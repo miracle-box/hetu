751c6dfb42464c84fea25ceb024bceb15a60ae30
Nixieboluo <me@sagirii.me>
2025-02-19 18:08:44 +0800

    refactor(web): static remote pattern config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [JSON.parse(process.env.TEXTURE_STORE_CONFIG)],
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: '**',
+				port: '',
+				pathname: '**',
+			},
+		],
 	},
 	logging: {
 		fetches: {
 			fullUrl: process.env.NODE_ENV === 'development',
 		},
+	},
+	// Type checking and linting should be in individual tasks.
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
 	},
 };
 
