2cc1ccc05638d1a2f18e50c2592f4e70f205fff5
Nixieboluo <me@sagirii.me>
2025-04-12 16:49:27 +0800

    refactor(ui): new form hook with better dx

--- packages/ui/src/hooks/use-app-form.ts
+++ packages/ui/src/hooks/use-app-form.ts
@@ -1,4 +1,11 @@
-import { createFormHookContexts, createFormHook } from '@tanstack/react-form';
+import {
+	createFormHookContexts,
+	createFormHook,
+	type FormOptions,
+	type FormAsyncValidateOrFn,
+	type FormValidateOrFn,
+} from '@tanstack/react-form';
+import React from 'react';
 import {
 	Form,
 	FormFieldControl,
@@ -14,7 +21,7 @@ import {
 export const { fieldContext, formContext, useFieldContext, useFormContext } =
 	createFormHookContexts();
 
-export const { useAppForm, withForm } = createFormHook({
+export const { useAppForm: appFormHook, withForm } = createFormHook({
 	fieldContext,
 	formContext,
 	fieldComponents: {
@@ -31,3 +38,64 @@ export const { useAppForm, withForm } = createFormHook({
 		Form,
 	},
 });
+
+export function createAppFormHook<
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	TFormView extends (props: any) => React.JSX.Element,
+	// Type params required by FormOptions
+	TFormData,
+	TOnMount extends undefined | FormValidateOrFn<TFormData>,
+	TOnChange extends undefined | FormValidateOrFn<TFormData>,
+	TOnChangeAsync extends undefined | FormAsyncValidateOrFn<TFormData>,
+	TOnBlur extends undefined | FormValidateOrFn<TFormData>,
+	TOnBlurAsync extends undefined | FormAsyncValidateOrFn<TFormData>,
+	TOnSubmit extends undefined | FormValidateOrFn<TFormData>,
+	TOnSubmitAsync extends undefined | FormAsyncValidateOrFn<TFormData>,
+	TOnServer extends undefined | FormAsyncValidateOrFn<TFormData>,
+	TSubmitMeta,
+>(
+	predefinedFormOptions: FormOptions<
+		TFormData,
+		TOnMount,
+		TOnChange,
+		TOnChangeAsync,
+		TOnBlur,
+		TOnBlurAsync,
+		TOnSubmit,
+		TOnSubmitAsync,
+		TOnServer,
+		TSubmitMeta
+	>,
+	FormView: TFormView,
+) {
+	return (
+		formOptions: FormOptions<
+			TFormData,
+			TOnMount,
+			TOnChange,
+			TOnChangeAsync,
+			TOnBlur,
+			TOnBlurAsync,
+			TOnSubmit,
+			TOnSubmitAsync,
+			TOnServer,
+			TSubmitMeta
+		>,
+	) => {
+		const form = appFormHook({ ...predefinedFormOptions, ...formOptions });
+		const formId = React.useId();
+
+		return {
+			form,
+			formId,
+			// Pre-defined form instance and formId for better DX.
+			FormView: (props: Omit<Parameters<TFormView>[0], 'form' | 'formId'>) => {
+				return FormView({
+					...props,
+					form,
+					formId,
+				});
+			},
+		};
+	};
+}
