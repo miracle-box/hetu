54863d78422050b68eff33978a8acea35453a4ee
Nixieboluo <me@sagirii.me>
2025-05-19 23:05:52 +0800

    feat(backend): print request id in the prettified log

--- apps/backend/src/shared/middlewares/request-id/index.ts
+++ apps/backend/src/shared/middlewares/request-id/index.ts
@@ -1,11 +1,17 @@
+import { AsyncLocalStorage } from 'node:async_hooks';
 import { createId } from '@paralleldrive/cuid2';
 import Elysia from 'elysia';
 
 const HEADER_KEY = 'X-Request-ID';
 
+// [TODO] This probably should be moved outside of this middleware.
+export const requestIdStore = new AsyncLocalStorage<string>();
+
 export const requestId = new Elysia({ name: 'RequestId' })
 	.onRequest(({ set, request: { headers } }) => {
-		set.headers[HEADER_KEY] = headers.get(HEADER_KEY) ?? createId();
+		const requestId = headers.get(HEADER_KEY) ?? createId();
+		set.headers[HEADER_KEY] = requestId;
+		requestIdStore.enterWith(requestId);
 	})
 	.derive(({ set }) => {
 		return {
